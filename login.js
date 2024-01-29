c76b1a3839e5b075258e0502affe49383d444d8e
kalinzt <kalinzt@gmail.com>
2024-01-29 17:13:15 +0900

    commit main

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-const { sleep, setWaitTimeout, waitForElement } = require('wd/lib/commands');
+const { sleep } = require('wd/lib/commands');
 const { remote } = require('webdriverio');
 
 async function runLogin() {
@@ -26,37 +26,54 @@ async function runLogin() {
 
     try {
         // 로그인
-        // await driver.waitForElement("xpath://android.view.View[@content-desc=\"이메일\n비밀번호\"]".isDisplayed, 10000, 100);
-        const log_Id = await driver.$("xpath://android.view.View[@content-desc=\"이메일\n비밀번호\"]");
-        await log_Id.waitForDisplayed();
+        const loginBtn = await driver.$('//android.view.View[@content-desc="로그인"]').waitForExist({ timeout: 10000, interval: 3000 });
+        const log_Id = await driver.$('//android.view.View[@content-desc=\"이메일\n비밀번호\"]/android.widget.EditText[1]');
         await log_Id.click();
         await log_Id.addValue("jaden_s1@seoltab.test"); // 로그인 아이디 입력
-        const log_Pw = await driver.$("xpath://android.view.View[@content-desc=\"이메일\n비밀번호\"]/android.widget.EditText[2]");
+        const log_Pw = await driver.$('//android.view.View[@content-desc=\"이메일\n비밀번호\"]/android.widget.EditText[2]');
         await log_Pw.click();
         await log_Pw.addValue("asdfasdf"); // 로그인 패스워드 입력
         const log_Btn = await driver.$("accessibility id:로그인");
         await log_Btn.click(); // 로그인 버튼 클릭
 
         // 튜토리얼 컨트롤
-        console.log('튜토리얼을 제어 합니다.')
-        let count = 0;
-        while (count < 10) {
-            await browser.touchAction([
-                { action: 'press', x: 700, y: 150 },
-                { action: 'release' }
-            ]);
+        // await browser.action('pointer', {
+        //     parameters: { pointerType: 'touch' }
+        // })
+        // console.log('튜토리얼을 제어 합니다.')
+
+        // let count = 0;
+        // while (count < 10) {
+        //     await browser.touchAction([
+        //         { action: 'press', x: 700, y: 150 },
+        //         { action: 'release' }
+        //     ]);
+        // }
+        const waitMypage = await
+            driver.$('//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View')
+                .waitForDisplayed(10000);
+        let t = 0;
+        const maxTaps = 12;
+        const screen = await driver.$('//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View');
+        await screen.touchAction('tap');
+
+        while (t <= maxTaps) {
+            await screen.touchAction({ action: 'tap' });
+            t++;
+            await sleep(1000); // 1초 대기
         }
 
         // 로그아웃
-        const gnb_Mypage = await driver.$("xpath://android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView");
+        const gnb_Mypage = await driver.$('//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout[1]/android.widget.FrameLayout/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.ImageView');
         await gnb_Mypage.click(); // GNB 마이페이지 버튼 클릭
-        const mypage_logOut = await driver.$("id:com.seoltab.seoltab:id/tvLogout");
+        const mypage_logOut = await driver.$('//android.widget.TextView[@resource-id="com.seoltab.seoltab:id/tvLogout"]');
         await mypage_logOut.click(); // 마이페이지 내 로그아웃 버튼 클릭
-        const logoutChk = document.getElementById("//android.view.View[@content-desc=\"로그인\"]")
-        const chktext = ('로그인');
+        await sleep(3000);
+        const logoutChk = await (await driver.$('//android.view.View[@content-desc=\"로그인\"]')).getTagName();
+        const chktext = '로그인';
         if (chktext == logoutChk) {
-            console.log('로그아웃 되었습니다.')
-        }
+            console.log('로그인 버튼이 확인 되었습니다.')
+        } else console.log('로그인 버튼의 텍스트를 가져오지 못했습니다.')
 
     } finally {
         await driver.pause(1000);
